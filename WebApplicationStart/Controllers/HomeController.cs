afd03c3550091c41a46f3688151bf1ee924e7ccc
Stas Persaev <persaev10@mail.ru>
2025-03-12 10:56:41 +0300

    Добавил значения по умолчанию для вещественных чисел в объявлении метода Task3

--- WebApplicationStart/Controllers/HomeController.cs
+++ WebApplicationStart/Controllers/HomeController.cs
@@ -4,72 +4,72 @@ using WebApplicationStart.Models;
 
 namespace WebApplicationStart.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+		public IActionResult Index()
+		{
+			return View();
+		}
 
 
-        public IActionResult Tasks()
-        {
-            return View();
-        }
+		public IActionResult Tasks()
+		{
+			return View();
+		}
 
-        public string Task1()
-        {
-            var timeInHour = DateTime.Now.Hour;
-            if (timeInHour < 6)
-                return "Доброй ночи";
-            if (timeInHour < 12)
-                return "Доброе утро";
-            if (timeInHour < 18)
-                return "Добрый день";
-            return "Добрый вечер";
-        }
+		public string Task1()
+		{
+			var timeInHour = DateTime.Now.Hour;
+			if (timeInHour < 6)
+				return "Доброй ночи";
+			if (timeInHour < 12)
+				return "Доброе утро";
+			if (timeInHour < 18)
+				return "Добрый день";
+			return "Добрый вечер";
+		}
 
-        public string Task2(double a, double b)
-        {
-            return $"{a} + {b} = {a + b}";
-        }
+		public string Task2(double a, double b)
+		{
+			return $"{a} + {b} = {a + b}";
+		}
 
-        public string Task3(double a, double b, char op = '+')
-        {
-            switch (op)
-            {
-                case '+': return (a + b).ToString();
-                case '-': return (a - b).ToString();
-                case '*': return (a * b).ToString();
-                default:
-                    return "Данная операция недоступна.\nСущетсвует возможность только ввода '+', '-' и '*'";
-            }
-        }
+		public string Task3(double a = 0, double b = 0, char op = '+')
+		{
+			switch (op)
+			{
+				case '+': return (a + b).ToString();
+				case '-': return (a - b).ToString();
+				case '*': return (a * b).ToString();
+				default:
+					return "Данная операция недоступна.\nСущетсвует возможность только ввода '+', '-' и '*'";
+			}
+		}
 
-        public string Task4(double a, double b, char op = '+')
-        {
-            switch (op)
-            {
-                case '+': return $"{a} + {b} = {a + b}";
-                case '-': return $"{a} - {b} = {a - b}";
-                case '*': return $"{a} * {b} = {a * b}";
-                case '/': return b != 0 ? $"{a} / {b} = {a / b}" : "Делить на 0 нельзя!";
-                default:
-                    return "Данная операция недоступна.\nСущетсвует возможность только ввода '+', '-' и '*', '/'";
-            }
-        }
+		public string Task4(double a, double b, char op = '+')
+		{
+			switch (op)
+			{
+				case '+': return $"{a} + {b} = {a + b}";
+				case '-': return $"{a} - {b} = {a - b}";
+				case '*': return $"{a} * {b} = {a * b}";
+				case '/': return b != 0 ? $"{a} / {b} = {a / b}" : "Делить на 0 нельзя!";
+				default:
+					return "Данная операция недоступна.\nСущетсвует возможность только ввода '+', '-' и '*', '/'";
+			}
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
