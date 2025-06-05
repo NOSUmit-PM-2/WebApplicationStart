292b3a03383e395614f6c6bd2ad9273505937a75
aanokodzikiotaku@gmail.com <aanokodzikiotaku@gmail.com>
2025-06-05 15:00:55 +0300

    сделал задания с 1 по 4, только 3 и 4 вместе

--- WebApplicationStart/Controllers/HomeController.cs
+++ WebApplicationStart/Controllers/HomeController.cs
@@ -24,9 +24,33 @@ namespace WebApplicationStart.Controllers
             return View();
         }
 
+        //задача 1
         public string Task1()
         {
-            return "Тут решение первой задачи";
+            var Hour = DateTime.Now.Hour;
+
+            if (Hour >= 0 && Hour < 6) return "Ночь";
+            if (Hour >= 6 && Hour < 12) return "Утро";
+            if (Hour >= 12 && Hour < 18) return "День";
+
+            return "Вечер";
+        }
+
+        //задача 2
+        public string Task2(int a, int b)
+        {
+            return Convert.ToString(a + b);
+        }
+
+        //решение задач 3 и 4
+        public string Task3_4(int a, int b, string sign)
+        {
+            if (sign == "+") return Convert.ToString(a + b);
+            if (sign == "-") return Convert.ToString(a - b);
+            if (sign == "*") return Convert.ToString(a * b);
+            if (sign == "/") return Convert.ToString(a / b);
+            else return "Ошибочка";
+
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
