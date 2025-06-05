b3b0da9a9ff4218ec426be329d5a034d106a09fc
abazovaslan72@gmail.com <abazovaslan72@gmail.com>
2025-06-05 16:33:39 +0300

    задание 6. создал папку data а вней UserRepository. UserController и две вьюшки. выводит таблицу юзеров по нажатию открывает карточку отдельного юзера. поиск по имяни.

--- WebApplicationStart/Controllers/UserController.cs
+++ WebApplicationStart/Controllers/UserController.cs
@@ -1,13 +1,67 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationStart.Data;
 using WebApplicationStart.Models;
 
 namespace WebApplicationStart.Controllers
 {
     public class UserController : Controller
     {
-        public IActionResult Index(int id , string name, string email, string password,string login,string telephone)
+        private readonly UserRepository _userRepository;
+
+        public UserController()
         {
-            var user = new User(id,name,email,password,login,telephone);
+            _userRepository = new UserRepository();
+        }
+
+        public IActionResult Index(string searchName)
+        {
+            // Если есть поисковый запрос
+            if (!string.IsNullOrEmpty(searchName))
+            {
+                var foundUser = _userRepository.GetAll()
+                    .FirstOrDefault(u => u.Name.Contains(searchName, StringComparison.OrdinalIgnoreCase));
+
+                if (foundUser != null)
+                {
+                    return View(new List<User> { foundUser });
+                }
+
+                ViewBag.Message = "Пользователь не найден";
+                return View(new List<User>());
+            }
+
+            // Если поиска нет - возвращаем всех пользователей
+            return View(_userRepository.GetAll());
+        }
+
+        public IActionResult Details(int id)
+        {
+            var user = _userRepository.TryGetById(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                // Генерируем новый ID
+                var newId = _userRepository.GetAll().Max(u => u.IdUser) + 1;
+                user.IdUser = newId;
+
+                _userRepository.Add(user);
+                return RedirectToAction("Index");
+            }
             return View(user);
         }
     }
---
+++ WebApplicationStart/Data/UserRepository.cs
@@ -0,0 +1,35 @@
+﻿using WebApplicationStart.Models;
+
+namespace WebApplicationStart.Data
+{
+    public class UserRepository
+    {
+        public static List<User> users = new List<User>()
+        {
+            new User(1, "Иван Иванов", "ivan@mail.com", "qwerty123", "ivanov", "+79161234567"),
+            new User(2, "Петр Петров", "petr@mail.com", "pass1234", "petrov", "+79261234568"),
+            new User(3, "Анна Сидорова", "anna@mail.com", "anna2023", "sidorova", "+79361234569"),
+            new User(4, "Мария Кузнецова", "maria@mail.com", "maria777", "kuznetsova", "+79461234570"),
+            new User(5, "Алексей Смирнов", "alex@mail.com", "smirnoff", "smirnov", "+79561234571"),
+            new User(6, "Елена Волкова", "elena@mail.com", "volkova1", "volkova", "+79661234572"),
+            new User(7, "Дмитрий Козлов", "dmitry@mail.com", "kozlovdd", "kozlov", "+79761234573"),
+            new User(8, "Ольга Новикова", "olga@mail.com", "novikova8", "novikova", "+79861234574"),
+            new User(9, "Сергей Морозов", "sergey@mail.com", "frost2023", "morozov", "+79961234575"),
+            new User(10, "Наталья Павлова", "nataly@mail.com", "pavlovan", "pavlova", "+79061234576")
+        };
+        public List<User> GetAll()
+        {
+            return users;
+        }
+
+        public User TryGetById(int id)
+        {
+            return users.FirstOrDefault(x => x.IdUser == id);
+        }
+
+        public void Add(User user)
+        {
+            users.Add(user);
+        }
+    }
+}
