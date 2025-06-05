4d5f47367aa77c039cc34809699eec7e28e5616d
abazovaslan72@gmail.com <abazovaslan72@gmail.com>
2025-06-05 16:10:39 +0300

    Задание 5

---
+++ WebApplicationStart/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationStart.Models;
+
+namespace WebApplicationStart.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index(int id , string name, string email, string password,string login,string telephone)
+        {
+            var user = new User(id,name,email,password,login,telephone);
+            return View(user);
+        }
+    }
+}
---
+++ WebApplicationStart/Models/User.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApplicationStart.Models
+{
+    public class User
+    {
+        public int IdUser;
+        public string Name;
+        public string Email;
+        public string Password;
+        public string Login;
+        public string Telephone;
+
+        public User(int idUser, string name, string email, string password, string login, string telephone)
+        {
+            IdUser = idUser;
+            Name = name;
+            Email = email;
+            Password = password;
+            Login = login;
+            Telephone = telephone;
+        }
+    }
+}
