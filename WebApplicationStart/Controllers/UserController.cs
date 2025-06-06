b27ea1007d2ca8fbe3fa018bcba5f9f1867aa5cf
tsaga <geor.tsaga@yandex.ru>
2025-06-06 11:29:46 +0300

    решение задачи 5

---
+++ WebApplicationStart/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationStart.Models;
+
+namespace WebApplicationStart.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            UserViewModel testUser = new UserViewModel("Test", "keks@mail.com", "password", "+77777777");
+            return View(testUser);
+        }
+    }
+}
