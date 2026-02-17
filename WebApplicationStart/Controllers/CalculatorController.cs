fefd0a231d8705a514729d9108121d15b1ce5e9d
AmletixQ <nazarovamridin308@gmail.com>
2026-02-17 10:22:06 +0300

    Решение третьей задачи

--- WebApplicationStart/Controllers/CalculatorController.cs
+++ WebApplicationStart/Controllers/CalculatorController.cs
@@ -4,9 +4,30 @@ namespace WebApplicationStart.Controllers
 {
     public class CalculatorController : Controller
     {
-        public IActionResult Index(int a, int b)
+        public IActionResult Index(int a, int b, char operation)
         {
-            string result = $"{a} + {b} = {a + b}";
+            if (operation != 0 && !"!=-*".Contains(operation))
+                throw new Exception("Invalid operation");
+
+            string result;
+
+            switch (operation)
+            {
+                case '+':
+                    result = $"{a} + {b} = {a + b}";
+                    break;
+                case '-':
+                    result = $"{a} - {b} = {a - b}";
+                    break;
+                case '*':
+                    result = $"{a} * {b} = {a * b}";
+                    break;
+                default:
+                    result = $"{a} + {b} = {a + b}";
+                    break;
+            }
+
+            
             return View(result as object);
         }
     }
