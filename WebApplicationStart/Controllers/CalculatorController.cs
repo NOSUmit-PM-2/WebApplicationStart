596663bd9c85f68c5cdc20a445a586514af0f338
Soqyaro <chopik531@gmail.com>
2026-02-22 16:00:19 +0300

    Добавил контроллер для операции сложения чисел, переданных в двух последних сегментах запроса. Изменил маршрутизацию паттерна

---
+++ WebApplicationStart/Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(int a, int b)
+        {
+            return (a + b).ToString();
+        }
+    }
+}
