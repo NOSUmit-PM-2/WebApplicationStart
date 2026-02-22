4f7a9b04264857e79273edb1e1d42e88e5931682
Soqyaro <chopik531@gmail.com>
2026-02-22 15:30:04 +0300

    Реализован контроллер StartController и метод Hello. Добавлена логика определения времени суток. Создано представление Hello.cshtml. Приветствие передаётся во View через ViewBag.

---
+++ WebApplicationStart/Controllers/StartController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    public class StartController : Controller
+    {
+        public IActionResult Hello()
+        {
+            int hour = DateTime.Now.Hour;
+            string temp;
+
+            if (hour >= 0 && hour < 6)
+            {
+                temp = "Доброй ночи";
+            }
+            else if (hour >= 6 && hour < 12)
+            {
+                temp = "Доброе утро";
+            }
+            else if (hour >= 12 && hour < 18)
+            {
+                temp = "Добрый день";
+            }
+            else
+            {
+                temp = "Добрый вечер";
+            }
+            ViewBag.Greeting = temp;
+            return View();
+        }
+    }
+}
