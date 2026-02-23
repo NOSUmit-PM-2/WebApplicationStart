cfd8e3e0547112cf2a24735062556929402746a1
ParzivaloC <bicherahovartur12@mail.ru>
2026-02-23 22:30:16 +0300

    Задание 1 Доброе утро, Добрый день, Добрый вечер или Доброй ночи

---
+++ WebApplicationStart/Controllers/StartController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    public class StartController : Controller
+    {
+        public string Index()
+        {
+            var hour = DateTime.Now.Hour;
+
+            if (hour >= 0 && hour < 6)
+            {
+                return "Доброй ночи";
+            }
+            if (hour >= 6 && hour < 12)
+            {
+                return "Доброе утро";
+            }
+            if (hour >= 12 && hour < 18)
+            {
+                return "Добрый день";
+            }
+
+            return "Добрый вечер";
+        }
+    }
+}
