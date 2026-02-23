9ba2ba7ddcb2dffa6799b117168da6aa7d43d7bf
Элина Кокоева <yummysalt@bk.ru>
2026-02-23 23:28:44 +0300

    Кокоева. Решено задание 3

---
+++ WebApplicationStart/Controllers/CalculatorController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplicationStart.Models;
+
+namespace WebApplicationStart.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public string Index(int a = 0, int b = 0, string oper = "+")
+        {
+            switch (oper)
+            {
+                case "+": return $"{a} + {b} = {a + b}";
+				case "-": return $"{a} - {b} = {a - b}";
+				case "*": return $"{a} * {b} = {a * b}";
+			}
+            return "";
+        }
+    }
+}
