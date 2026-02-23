25bc72e7d795dc050c02506cc44c3b1a92146891
ParzivaloC <bicherahovartur12@mail.ru>
2026-02-23 23:22:42 +0300

    Задание 3 калькулятор

---
+++ WebApplicationStart/Controllers/CalculatorController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(double a, double b, string operation)
+        {
+            double result;
+            switch (operation)
+            {
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                default:
+                    operation = "+";
+                    result = a + b;
+                    break;
+            }
+            return $"{a} {operation} {b} = {result}";
+        }
+    }
+}
