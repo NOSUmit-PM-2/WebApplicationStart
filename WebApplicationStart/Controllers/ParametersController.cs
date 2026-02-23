d781da4faa0aef8ab4d9e2b8c980aedb94c1cfb8
ParzivaloC <bicherahovartur12@mail.ru>
2026-02-23 22:55:09 +0300

    Задание 2 Параметры

---
+++ WebApplicationStart/Controllers/ParametersController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using System.Net.WebSockets;
+
+namespace WebApplicationStart.Controllers
+{
+    public class CalcController : Controller
+    {
+        public string Index(double a, double b )
+        {
+            var sum = a + b;
+            return $"{a} + {b} = {sum}";
+        }
+    }
+}
--- WebApplicationStart/Controllers/StartController.cs
+++ WebApplicationStart/Controllers/StartController.cs
@@ -4,24 +4,24 @@ namespace WebApplicationStart.Controllers
 {
     public class StartController : Controller
     {
-        public string Index()
-        {
-            var hour = DateTime.Now.Hour;
+        //public string Index()
+        //{
+        //    var hour = DateTime.Now.Hour;
 
-            if (hour >= 0 && hour < 6)
-            {
-                return "Доброй ночи";
-            }
-            if (hour >= 6 && hour < 12)
-            {
-                return "Доброе утро";
-            }
-            if (hour >= 12 && hour < 18)
-            {
-                return "Добрый день";
-            }
+        //    if (hour >= 0 && hour < 6)
+        //    {
+        //        return "Доброй ночи";
+        //    }
+        //    if (hour >= 6 && hour < 12)
+        //    {
+        //        return "Доброе утро";
+        //    }
+        //    if (hour >= 12 && hour < 18)
+        //    {
+        //        return "Добрый день";
+        //    }
 
-            return "Добрый вечер";
-        }
+        //    return "Добрый вечер";
+        //}
     }
 }
