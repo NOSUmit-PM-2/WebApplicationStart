01e63fac54b1a20bf4826bb4edcd67110be52079
workdzhanaev <workdzhanaev@gmail.com>
2026-02-26 20:57:02 +0300

    Решение 3 задачи

--- WebApplicationStart/Controllers/CalculateController.cs
+++ WebApplicationStart/Controllers/CalculateController.cs
@@ -4,9 +4,17 @@ namespace WebApplicationStart.Controllers
 {
     public class CalculateController : Controller
     {
-        public string Index(int n1, int n2)
+        
+        public string Task3(double n1, double n2, string operation = "+")
         {
-            return $"{n1}+{n2}={n1 + n2}";
+            switch (operation)
+            {
+                case "+": return $"{n1} + {n2} = {n1 + n2}";
+                case "*": return $"{n1} * {n2} = {n1 * n2}";               
+                case "-": return $"{n1} - {n2} = {n1 - n2}";
+                default: return $"Ошибка: Некорректная операция. Допустимые операции: +, -, *";
+            }
+
         }
     }
 }
