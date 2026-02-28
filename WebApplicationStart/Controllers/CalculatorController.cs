401ae247b6ce7a35d70239b60800cbb3e8a7643a
flex12312 <davidtavk06@mail.ru>
2026-02-28 20:00:05 +0300

    решение задачи 3

--- WebApplicationStart/Controllers/CalculatorController.cs
+++ WebApplicationStart/Controllers/CalculatorController.cs
@@ -4,9 +4,16 @@ namespace WebApplicationStart.Controllers
 {
     public class CalculatorController : Controller
     {
-        public string Index(int a = 0, int b = 0)
+        public string Index(int a = 0, int b = 0, string operation = "+")
         {
-            return $"{a} + {b} = {a+b}";
+            switch (operation)
+            {
+                case "+": return $"{a}+{b}={a + b}";
+                case "-": return $"{a}-{b}={a - b}";
+                case "*": return $"{a}*{b}={a * b}";
+                case "/": return $"{a}/{b}={a / b}";
+                default: return "Ошибка: Некорректная операция. Допустимые операции: +, -, *\rПример корректного запроса: https://localhost:5090/calculator/index/1/3/+";
+            }
         }
     }
 
