c80759e9224a9ede55c5146c930382b13f215a26
LeraKhubulova <lera.khubulova.06@mail.ru>
2026-03-02 00:35:51 +0300

    выполнила второе задание

---
+++ WebApplicationStart/Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(double num1, double num2)
+        {
+            return $"{num1} + {num2} = {num1 + num2}";
+        }
+    }
+}
