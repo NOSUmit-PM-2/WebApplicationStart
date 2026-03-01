25cd02022f439cd8fc18bd25f23504ab0751692d
LeraKhubulova <lera.khubulova.06@mail.ru>
2026-03-02 00:06:47 +0300

    выполнила первое задание

---
+++ WebApplicationStart/Controllers/StartController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    public class StartController : Controller
+    {
+        public string Hello()
+        {
+            var currentHour=DateTime.Now.Hour;
+
+            if (currentHour < 6)
+            {
+                return "Доброй ночи";
+            }
+
+            if (currentHour <12)
+            {
+                return "Доброе утро";
+            }
+
+            if (currentHour < 18)
+            {
+                return "Добрый день";
+            }
+
+            return "Добрый вечер";            
+        }
+    }
+}
