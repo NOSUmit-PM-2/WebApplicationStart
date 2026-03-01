e317976425b778e4e0beb0a9a619cb71556838d6
яна кайтукова <yanakaytuk@gmail.com>
2026-03-01 20:14:21 +0300

    задание 4 выполнено

---
+++ WebApplicationStart/Controllers/CalcController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplicationStart.Models;
+
+namespace WebApplicationStart.Controllers
+{
+    public class CalcController : Controller
+    {
+        public string Index(double a=0, double b = 0, string c="+")
+        {
+            switch (c)
+            {
+                case "+":
+                    return $"{a} + {b} = {a + b}";
+                case "-":
+                    return $"{a} - {b} = {a - b}";
+                case "*":
+                    return $"{a} * {b} = {a * b}";
+                case "/":
+                    if (b == 0)
+                    {
+                        Console.WriteLine("Ошибка: деление на ноль!");
+                    }
+                    return $"{a} / {b} = {a / b}";
+                default:
+                    return "Ошибка: неверный оператор!";
+            }
+        }
+    }
+
+}
