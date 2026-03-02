d9077a4752d20ef80f441168e6da9f673e6f12f7
ISHADYI <shadyworkq@gmail.com>
2026-03-02 22:11:05 +0300

    Выполнение первого задания

---
+++ WebApplicationStart/Controllers/StartController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace WebApplicationStart.Controllers
+{
+    public class StartController : Controller
+    {
+        public string Hello()
+        {
+            int timeHour = DateTime.Now.Hour;
+            string hello = "";
+
+            switch(timeHour)
+            {
+                case int n when (n >= 0 && n < 6):
+                    hello = "Доброе утро";
+                    break;
+                case int n when (n >= 6 && n < 12):
+                    hello = "Добрый день";
+                    break;
+                case int n when (n >= 12 && n < 18):
+                    hello = "Добрый вечер";
+                    break;
+                case int n when (n >= 18 && n < 24):
+                    hello = "Доброй ночи";
+                    break;
+            }
+            return hello;
+        }
+    }
+}
