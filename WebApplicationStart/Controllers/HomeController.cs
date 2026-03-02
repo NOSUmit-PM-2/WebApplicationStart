20706b3822b84cbfbd522b6c59cb5e15bda63325
Makarenkomd <mahadrum@yandex.ru>
2026-03-02 23:33:27 +0300

    Update HomeController.cs

--- WebApplicationStart/Controllers/HomeController.cs
+++ WebApplicationStart/Controllers/HomeController.cs
@@ -6,13 +6,6 @@ namespace WebApplicationStart.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
@@ -22,17 +15,6 @@ namespace WebApplicationStart.Controllers
         public IActionResult Tasks()
         {
             return View();
-        }
-
-        public string Task1()
-        {
-            return "Тут решение первой задачи";
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
