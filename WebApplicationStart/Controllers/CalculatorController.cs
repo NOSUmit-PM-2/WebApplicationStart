6b40548f202f75e9898ee5dce4451e1a76762ea0
Ivanov Stas <toster.one@yandex.ru>
2026-03-02 23:01:47 +0300

    иванов 2 задание

---
+++ WebApplicationStart/Controllers/CalculatorController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplicationStart.Models;
+
+namespace WebApplicationStart.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public CalculatorController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public string Index(int first, int second)
+        {
+            return $"{first} + {second} = {first + second}";
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
