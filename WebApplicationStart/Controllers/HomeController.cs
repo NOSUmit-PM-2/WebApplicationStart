fe4e91db9e96908a5ee9abfca37bfd501b65035f
Geor Guzitaev <georguzitaev@gmail.com>
2026-03-03 17:13:36 +0300

    Сделал отдельный контроллер для калькулятора

--- WebApplicationStart/Controllers/HomeController.cs
+++ WebApplicationStart/Controllers/HomeController.cs
@@ -26,10 +26,7 @@ namespace WebApplicationStart.Controllers
 
        
 
-        public string Task2(int a, int b)
-        {
-            return $"{a + b}";
-        }
+        
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
