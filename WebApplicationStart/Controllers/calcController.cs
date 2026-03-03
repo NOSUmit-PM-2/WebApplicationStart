479a0a6358a7a9bf16b2f1cd71e81d06555d00c2
David <davidpliev2006@gmail.com>
2026-03-03 09:20:26 +0300

    Исправил запрос

---
+++ WebApplicationStart/Controllers/calcController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    public class calcController : Controller
+    {
+        public string Index(int a = 0, int b = 0, string c = "+")
+        {
+            switch (c)
+            {
+                case "+": return $"{a} + {b} = {a + b}";
+                case "-": return $"{a} - {b} = {a - b}";
+                case "*": return $"{a} * {b} = {a * b}";
+                case "/": if (b == 0) { return "На ноль делить нельзя"; } return $"{a} * {b} = {a * b}";
+                default: return $"Введите корректную операцию(+,-,*)";
+            }
+        }
+    }
+}
