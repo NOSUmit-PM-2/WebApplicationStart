8baa7a8ee03339dc87d4810bd6ce03e91fdfc8d6
David <davidpliev2006@gmail.com>
2026-03-03 09:14:09 +0300

    Исправил запрос

---
+++ WebApplicationStart/Controllers/calculatorController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    public class calculatorController : Controller
+    {
+        public string Index(int num1 = 0, int num2 = 0, string op = "+")
+        {
+            switch (op)
+            {
+                case "+": return $"{num1} + {num2} = {num1 + num2}";
+                case "-": return $"{num1} - {num2} = {num1 - num2}";
+                case "*": return $"{num1} * {num2} = {num1 * num2}";
+                default: return $"Введите корректную операцию(+,-,*)";
+            }
+        }
+    }
+}
