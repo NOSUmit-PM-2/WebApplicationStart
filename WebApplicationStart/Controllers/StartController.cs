9af0037b24d1017745e3c58579e9739e9b9a31e5
Soqyaro <chopik531@gmail.com>
2026-03-08 13:11:58 +0300

    1) поменял порт 2) решил 1 задачу с приветсвиями

---
+++ WebApplicationStart/Controllers/StartController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    public class StartController : Controller
+    {
+        public string Hello()
+        {
+            int hour = DateTime.Now.Hour;
+
+            if (hour >= 0 && hour <= 5)
+                return "Доброй ночи";
+
+            else if (hour <= 11)
+                return "Доброе утро";
+
+            else if (hour <= 17)
+                return "Добрый день";
+
+            else
+                return "Добрый вечер";
+        }
+    }
+}
