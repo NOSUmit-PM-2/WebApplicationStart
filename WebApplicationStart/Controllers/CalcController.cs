1771e31b20a6889aade128bc49932d4cc535ee0d
Снорлакс Рождественский <xmassnorlax@outlook.com>
2026-03-14 22:36:28 +0300

    добавил контроллер

---
+++ WebApplicationStart/Controllers/CalcController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    public class CalcController : Controller
+    {
+        public string Index(double a = 0, double b = 0, string c = "+")
+        {
+            double res = 0;
+            if (c == "+")
+            {
+                res = a + b;
+            }
+            else if (c == "-")
+            {
+                res = a - b;
+            }
+            else if (c == "*")
+            {
+                res = a * b;
+            }
+            else if (c == "/")
+            {
+                if (b == 0)
+                {
+                    return "На ноль делить нельзя";
+                }
+                res = a / b;
+            }
+            else
+            {
+                return "Недопустимая операция!\r\nВ параметре, в котором передается знак арифметической опреации,\r\nмогут быть указаны только следующие варианты символов: %2B, - , *, /.";
+            }
+            return $"{a} {c} {b} = {res}";
+        }
+    }
+}
--- WebApplicationStart/Controllers/HomeController.cs
+++ WebApplicationStart/Controllers/HomeController.cs
@@ -29,35 +29,7 @@ namespace WebApplicationStart.Controllers
             return "Тут решение первой задачи";
         }
 
-        public string Task4(double a = 0, double b = 0, string c = "+")
-        {
-            double res = 0;
-            if (c == "+")
-            {
-                res = a + b;
-            }
-            else if (c == "-")
-            {
-                res = a - b;
-            }
-            else if (c == "*")
-            {
-                res = a * b;
-            }
-            else if (c == "/")
-            {
-                if(b == 0)
-                {
-                    return "На ноль делить нельзя";
-                }
-                res = a / b;
-            }
-            else
-            {
-                return "Операция не поддерживается";
-            }
-            return $"{a} {c} {b} = {res}";
-        }
+       
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
