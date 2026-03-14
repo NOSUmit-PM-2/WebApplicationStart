a49fd0f2292198b96a672ac82ba228370e8fd5d4
workdzhanaev <workdzhanaev@gmail.com>
2026-03-14 20:05:46 +0300

    Решение 1 задачи

---
+++ WebApplicationStart/Controllers/StartController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    public class StartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Hello()
+        {
+            var time = DateTime.Now.Hour;
+            if (time < 6)
+                return "Доброй ночи";
+            if (time < 12)
+                return "Доброе утро";
+            if (time < 18)
+                return "Добрый день";
+            return "Добрый вечер";
+        }
+    }
+}
