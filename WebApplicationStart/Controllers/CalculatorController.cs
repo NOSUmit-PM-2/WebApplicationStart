fec3a0d7b08c1d7b3b77ca992851e915c75b439f
Снорлакс Рождественский <xmassnorlax@outlook.com>
2026-03-14 22:21:40 +0300

    добавил контроллер

---
+++ WebApplicationStart/Controllers/CalculatorController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(double num1 = 0, double num2 = 0, string op = "+")
+        {
+            double res = 0;
+            if (op == "+")
+            {
+                res = num1 + num2;
+            }
+            else if (op == "-")
+            {
+                res = num1 - num2;
+            }
+            else if (op == "*")
+            {
+                res = num1 * num2;
+            }
+            else
+            {
+                return "Операция не поддерживается";
+            }
+            return $"{num1} {op} {num2} = {res}";
+        }
+    }
+}
