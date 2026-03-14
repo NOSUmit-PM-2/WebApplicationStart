c7cad79c155048f279f3b3edf7bb8b0d38b721f4
workdzhanaev <workdzhanaev@gmail.com>
2026-03-14 20:23:40 +0300

    Решение 2 задачи

---
+++ WebApplicationStart/Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(int n1 = 0, int n2 = 0)
+        {
+            return $"{n1}+{n2}={n1 + n2}";
+        }
+    }
+}
