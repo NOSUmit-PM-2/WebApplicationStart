daec764e5ecf05d4bae32c52236fc2d3cae55101
ParzivaloC <bicherahovartur12@mail.ru>
2026-03-15 19:10:13 +0300

    Задание 4 (Необходимо производить операции сложения, вычитания, умножения и деления чисел и выводить ответ в браузер в виде строки. В параметрах запроса a и b передаются числа, в параметре с передается знак арифметической операции.)

---
+++ WebApplicationStart/Controllers/CalcController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    public class CalcController : Controller
+    {
+        public string Index(double a = 0, double b = 0, string c = "+")
+        {
+            double result;
+
+            switch (c)
+            {
+                case "+":
+                    result = a + b;
+                    return $"{a} + {b} = {result}";
+
+                case "-":
+                    result = a - b;
+                    return $"{a} - {b} = {result}";
+
+                case "*":
+                    result = a * b;
+                    return $"{a} * {b} = {result}";
+
+                case "/":
+                    if (b == 0)
+                    {
+                        return "На ноль делить нельзя!!!";
+                    }
+                    result = a / b;
+                    return $"{a} / {b} = {result}";
+
+                default:
+                    return $"Знак '{c}' не поддерживается. Используйте +, -, *, /";
+            }
+        }
+    }
+}
