21f63e09982e65e33a911a67bef74c73d0dfb052
Geor Guzitaev <georguzitaev@gmail.com>
2026-03-15 22:31:58 +0300

    Выполнил задание 2

---
+++ WebApplicationStart/Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public string Index(double a = 0, double b = 0)
+        {
+            return (a + b) + "";
+        }
+    }
+}
