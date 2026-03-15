5c35797c6b5a6058579ca4fe20da0e8d166e2e19
ParzivaloC <bicherahovartur12@mail.ru>
2026-03-15 17:59:51 +0300

    Задание 2 , исправил ошибки

---
+++ WebApplicationStart/Controllers/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    public class Calculator : Controller
+    {
+        public string Index(double a, double b)
+        {
+            var sum = a + b;
+            return $"{a} + {b} = {sum}";
+        }
+    }
+}
