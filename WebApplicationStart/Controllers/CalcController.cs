7ebed08a2b1882af0c2bbbf096167c7b6ef88f35
Geor Guzitaev <georguzitaev@gmail.com>
2026-03-15 22:40:24 +0300

    Выполнил задание 4

---
+++ WebApplicationStart/Controllers/CalcController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    public class CalcController : Controller
+    {
+        public string Index(double a = 0, double b = 0, char c = '+')
+        {
+            return c switch
+            {
+                '+' => (a + b).ToString(),
+                '*' => (a * b).ToString(),
+                '-' => (a - b).ToString(),
+                '/' => b == 0 ? "На ноль делить нельзя" : (a / b).ToString(),
+                _ => "Ошибочная операция"
+            };
+        }
+    }
+}
