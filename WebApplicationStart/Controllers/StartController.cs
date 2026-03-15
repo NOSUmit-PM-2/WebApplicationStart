85a08adec4a0ad140fa63d1d2a21b7c1e21e1a70
ParzivaloC <bicherahovartur12@mail.ru>
2026-03-15 17:43:57 +0300

    Исправил ошибки

--- WebApplicationStart/Controllers/StartController.cs
+++ WebApplicationStart/Controllers/StartController.cs
@@ -4,22 +4,13 @@ namespace WebApplicationStart.Controllers
 {
     public class StartController : Controller
     {
-        public string Index()
+        public string Hello()
         {
             var hour = DateTime.Now.Hour;
 
-            if (hour >= 0 && hour < 6)
-            {
-                return "Доброй ночи";
-            }
-            if (hour >= 6 && hour < 12)
-            {
-                return "Доброе утро";
-            }
-            if (hour >= 12 && hour < 18)
-            {
-                return "Добрый день";
-            }
+            if (hour < 6) return "Доброй ночи";
+            if (hour < 12) return "Доброе утро";
+            if (hour < 18) return "Добрый день";
 
             return "Добрый вечер";
         }
