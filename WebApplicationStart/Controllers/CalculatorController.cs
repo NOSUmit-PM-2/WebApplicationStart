2fa8172657fe9e1ff0a7b9725cac0db04e9b86f7
Anton Taraskin <akdh060909@gmail.com>
2026-03-17 02:08:03 +0300

    Задание 2.  Сложение

---
+++ WebApplicationStart/Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index(double a, double b)
+        {
+            return Content($"{a} + {b} = {a + b}");
+        }
+    }
+}
