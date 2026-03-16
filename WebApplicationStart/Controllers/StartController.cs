9a15bcef8fd95c2e222aa7d644de70aaf1132444
Элина Кокоева <e.kokoeva06@mail.ru>
2026-03-16 22:20:16 +0300

    создала контроллер

---
+++ WebApplicationStart/Controllers/StartController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplicationStart.Models;
+
+namespace WebApplicationStart.Controllers
+{
+    public class StartController : Controller
+    {
+       
+    }
+}
