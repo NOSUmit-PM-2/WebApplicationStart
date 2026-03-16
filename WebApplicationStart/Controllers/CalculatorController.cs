300280b00ff291d1cb74dfc95c4d93466b91ea62
AmletixQ <nazarovamridin308@gmail.com>
2026-03-16 21:58:44 +0300

    Четвертое задание

--- WebApplicationStart/Controllers/CalculatorController.cs
+++ WebApplicationStart/Controllers/CalculatorController.cs
@@ -10,6 +10,9 @@ namespace WebApplicationStart.Controllers
                 return $"{a} * {b} = {a * b}";
             else if (c == "-")
                 return $"{a} - {b} = {a - b}";
+            else if (c == "/")
+                if (b != 0) return $"{a} / {b} = {a / b}";
+                else return "На 0 делить нельзя";
 
             return $"{a} + {b} = {a + b}";
         }
