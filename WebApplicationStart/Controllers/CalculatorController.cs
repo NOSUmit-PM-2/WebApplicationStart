a60e21232497ba44884d156137aa9aed5da95608
Anton Taraskin <akdh060909@gmail.com>
2026-03-17 02:20:46 +0300

    Задание 3. Опепации сложения, умножения, вычитания

--- WebApplicationStart/Controllers/CalculatorController.cs
+++ WebApplicationStart/Controllers/CalculatorController.cs
@@ -4,9 +4,19 @@ namespace WebApplicationStart.Controllers
 {
     public class CalculatorController : Controller
     {
-        public IActionResult Index(double a, double b)
+        public IActionResult Index(double a, double b, string operation)
         {
-            return Content($"{a} + {b} = {a + b}");
+            double c = 0;
+            switch (operation)
+            {
+                case "+": c = a + b; break;
+                case "-": c = a - b; break;
+                case "*": c = a * b; break;
+                case null: c = a + b; operation = "+"; break;
+                default:
+                    return Content("Неправильно указана операция");
+            }
+            return Content($"{a} {operation} {b} = {c}");
         }
     }
 }
