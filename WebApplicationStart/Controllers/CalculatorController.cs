67ba7e8f727ed84a28b06f26bfe8d22769c8380f
AmletixQ <nazarovamridin308@gmail.com>
2026-03-16 21:54:18 +0300

    Третье задание

--- WebApplicationStart/Controllers/CalculatorController.cs
+++ WebApplicationStart/Controllers/CalculatorController.cs
@@ -4,8 +4,13 @@ namespace WebApplicationStart.Controllers
 {
     public class CalculatorController : Controller
     {
-        public string Index(double a = 0, double b = 0)
+        public string Index(double a = 0, double b = 0, string c = "+")
         {
+            if (c == "*")
+                return $"{a} * {b} = {a * b}";
+            else if (c == "-")
+                return $"{a} - {b} = {a - b}";
+
             return $"{a} + {b} = {a + b}";
         }
     }
