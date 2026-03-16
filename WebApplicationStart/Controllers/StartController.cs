a6215205a6428a465aeaf8c2cbe8410673784027
Anton Taraskin <akdh060909@gmail.com>
2026-03-17 01:58:43 +0300

    Задание 1. Приветствие

---
+++ WebApplicationStart/Controllers/StartController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationStart.Controllers
+{
+    public class StartController : Controller
+    {
+        public IActionResult Hello()
+        {
+            DateTime now = DateTime.Now;
+            string greeting;
+
+            if (now.Hour >= 0 && now.Hour < 6)
+            {
+                greeting = "Доброй ночи";
+            }
+            else if (now.Hour >= 6 && now.Hour < 12)
+            {
+                greeting = "Доброе утро";
+            }
+            else if (now.Hour >= 12 && now.Hour < 18)
+            {
+                greeting = "Добрый день";
+            }
+            else
+            {
+                greeting = "Добрый вечер";
+            }
+
+            return Content(greeting);
+        }
+    }
+}
